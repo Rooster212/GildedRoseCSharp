795f46e3376bec97b2f822e3b8d7fa8997ce7689
rooster212 <jamieroos1@gmail.com>
2020-01-23 20:37:45 +0000

    Add beginning of InventoryItem

---
+++ GildedRoseConsole/InventoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRoseConsole
+{
+    public class InventoryItem
+    {
+        public string Name { get; private set; }
+        public int SellIn { get; private set; }
+        public int Quality { get; private set; }
+
+        public InventoryItem(string name, int sellIn, int quality)
+        {
+            this.Name = name;
+            this.SellIn = sellIn;
+            this.Quality = quality;
+        }
+
+        public static void ProcessInventoryUpdate()
+        {
+            // TODO: process update
+        }
+    }
+}
